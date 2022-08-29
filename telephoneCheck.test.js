a6aac8fe9863513b5922831d284195c392f64927
Leonardo Cisternas <leocisternasa@gmail.com>
2022-08-29 10:29:28 -0400

    telephoneCheck Function to validate a a format of telephone with RegExp and test (jest)

--- telephoneCheck.test.js
+++ telephoneCheck.test.js
@@ -1,5 +1,36 @@
 function telephoneCheck(str) {
-  return str.length === 0 ? false : "is possible."
+
+
+  // phoneFormats = "555-555-5555",
+  //   "(555)555-5555",
+  //   "(555) 555-5555",
+  //   "555 555 5555",
+  //   "5555555555",
+  //   "1 555 555 5555"
+
+  const regExPatterns = [
+    //555-555-5555
+    /^\d{3}-\d{3}-\d{4}$/,
+    //1 555-555-5555
+    /^1 \d{3}-\d{3}-\d{4}$/,
+    //1 (555) 555-5555
+    /^1 \(\d{3}\) \d{3}-\d{4}$/,
+    //5555555555
+    /^\d{10}$/,
+    //(555)555-5555
+    /^\(\d{3}\)\d{3}-\d{4}$/,
+    //"1(555)555-5555"
+    /^1\(\d{3}\)\d{3}-\d{4}$/,
+    //1 555 555 5555
+    /^1 \d{3} \d{3} \d{4}$/
+  ]
+
+
+  return regExPatterns.some(pattern => pattern.test(str));
+
+
+
+
 }
 
 
@@ -28,4 +59,8 @@ test('telephoneCheck', () => {
 
 test('telephoneCheck', () => {
   expect(telephoneCheck("11 555-555-5555")).toStrictEqual(false);
+});
+
+test('telephoneCheck', () => {
+  expect(telephoneCheck("123**&!!asdf#")).toStrictEqual(false);
 });
